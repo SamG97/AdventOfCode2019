80b576e9ecbf18952272447849a6ea62099e0fed
SamG97 <samgooch97@googlemail.com>
2019-12-08 12:34:28 +0000

    Added first weekend solutions

---
+++ code/day7.py
@@ -0,0 +1,83 @@
+import itertools
+
+
+def execute(program, inputs, pc=0):
+    while True:
+        instruction = [c for c in str(program[pc])]
+        while len(instruction) < 5:
+            instruction.insert(0, "0")
+        opcode = int(instruction[-2] + instruction[-1])
+
+        if opcode == 99:
+            return "HALT", pc
+
+        immediate_1 = instruction[2] == "1"
+        immediate_2 = instruction[1] == "1"
+        # immediate_3 never set
+
+        if immediate_1:
+            left = program[pc + 1]
+        else:
+            left = program[program[pc + 1]]
+        if opcode != 3 and opcode != 4:
+            if immediate_2:
+                right = program[pc + 2]
+            else:
+                right = program[program[pc + 2]]
+            res = program[pc + 3]
+        if opcode == 1:  # Plus
+            program[res] = left + right
+        elif opcode == 2:  # Multiply
+            program[res] = left * right
+        elif opcode == 3:  # Input
+            program[program[pc + 1]] = inputs.pop(0)
+        elif opcode == 4:  # Output
+            return left, pc + 2
+        elif opcode == 5:  # Branch if true
+            if left != 0:
+                pc = right
+            else:
+                pc += 3
+        elif opcode == 6:  # Branch if false
+            if left == 0:
+                pc = right
+            else:
+                pc += 3
+        elif opcode == 7:  # Less than
+            program[res] = 1 if left < right else 0
+        elif opcode == 8:  # Equals
+            program[res] = 1 if left == right else 0
+        else:
+            raise RuntimeError(f"Unknown opcode {opcode} at pc {pc}")
+
+        if opcode in [1, 2, 7, 8]:
+            pc += 4
+        elif opcode == 3 or opcode == 4:
+            pc += 2
+
+
+if __name__ == "__main__":
+    with open("../input/day7.txt") as f:
+        program_text = f.readline()
+
+    max_output = 0
+    for phases in itertools.permutations(list(range(5, 10))):
+        programs = [[int(val) for val in program_text.split(",")] for _ in range(5)]
+        pcs = [0] * 5
+        first_iter = True
+        output = 0
+        final_output = 0
+        while True:
+            for i, (program, phase) in enumerate(zip(programs, phases)):
+                if first_iter:
+                    output, pc = execute(program, [phase, output], pc=pcs[i])
+                else:
+                    output, pc = execute(program, [output], pc=pcs[i])
+                pcs[i] = pc
+            first_iter = False
+            if output == "HALT":
+                break
+            else:
+                final_output = output
+        max_output = max(final_output, max_output)
+    print(max_output)
---
+++ code/day8.py
@@ -0,0 +1,58 @@
+from collections import defaultdict
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def count_layers(image, width, height):
+    num_layers = len(image) / (width * height)
+    assert num_layers % 1 == 0
+    num_layers = int(num_layers)
+    offset = 0
+    counts = []
+    for _ in range(num_layers):
+        count = defaultdict(lambda: 0)
+        for _ in range(width * height):
+            val = int(image[offset])
+            count[val] += 1
+            offset += 1
+        counts.append(count)
+    assert offset == len(image)
+
+    min_zeros = float("inf")
+    min_layer = -1
+    for i, layer in enumerate(counts):
+        layer_zeros = layer[0]
+        if layer_zeros < min_zeros:
+            min_zeros = layer_zeros
+            min_layer = i
+    return counts[min_layer][1] * counts[min_layer][2]
+
+
+def render_image(image, width, height):
+    num_layers = len(image) / (width * height)
+    assert num_layers % 1 == 0
+    num_layers = int(num_layers)
+    final_image = [2] * (width * height)
+    for layer in range(num_layers):
+        offset = layer * width * height
+        for i in range(width * height):
+            if final_image[i] == 2:
+                final_image[i] = int(image[offset + i])
+
+    return final_image
+
+
+def print_image(image, width, height):
+    data = np.zeros((height, width, 3), dtype=np.uint8)
+    for h in range(height):
+        for w in range(width):
+            index = h * width + w
+            if image[index] == 1:
+                data[h, w] = [255] * 3
+    plt.imshow(data, interpolation='nearest')
+    plt.show()
+
+
+if __name__ == "__main__":
+    with open("../input/day8.txt") as f:
+        print_image(render_image(f.readline().strip("\n"), 25, 6), 25, 6)
