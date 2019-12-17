c64e44f2c1b3f60f1e20c0cbc71911f5ff7b458b
Sam Gooch <samgooch97@googlemail.com>
2019-12-17 23:40:27 +0000

    Day 16 part 2

--- code/day16.py
+++ code/day16.py
@@ -1,3 +1,5 @@
+import math
+
 base_pattern = [0, 1, 0, -1]
 
 
@@ -16,13 +18,31 @@ def apply_phase(in_signal):
 
 
 def calculate_fft(in_num, num_steps):
-    signal = [int(c) for c in str(in_num)]
+    signal = [int(c) for c in in_num]
     for _ in range(num_steps):
         signal = apply_phase(signal)
     return "".join([str(d) for d in signal])[:8]
 
 
+def calculate_offset_fft(in_num, num_steps):
+    offset = int(in_num[:7])
+    in_length = len(in_num)
+    full_length = in_length * 10000
+    assert offset > full_length / 2, "Only works when offset in second half"
+    digits_to_calc = full_length - offset
+    num_copies = math.ceil(digits_to_calc / in_length)
+    signal = [int(c) for c in in_num] * num_copies
+    signal = signal[-digits_to_calc:]
+
+    for _ in range(num_steps):
+        out_val = 0
+        for idx in range(digits_to_calc - 1, -1, -1):
+            out_val += signal[idx]
+            signal[idx] = abs(out_val) % 10
+    return "".join([str(d) for d in signal])[:8]
+
+
 if __name__ == "__main__":
     with open("../input/day16.txt") as f:
-        num = int(f.readline())
-    print(calculate_fft(num, 100))
+        num = f.readline().strip("\n")
+    print(calculate_offset_fft(num, 100))
